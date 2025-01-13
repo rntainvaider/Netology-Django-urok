56ff1c7a8f879398e335c7cad9498a4526c2e273
evgenii <vovk.evgenii.199811@gmail.com>
2025-01-13 16:50:52 +0300

    работа над вторым заданием

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,20 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
+    "omlet": {
+        "яйца, шт": 2,
+        "молоко, л": 0.1,
+        "соль, ч.л.": 0.5,
     },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
+    "pasta": {
+        "макароны, г": 0.3,
+        "сыр, г": 0.05,
     },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
+    "buter": {
+        "хлеб, ломтик": 1,
+        "колбаса, ломтик": 1,
+        "сыр, ломтик": 1,
+        "помидор, ломтик": 1,
     },
     # можете добавить свои рецепты ;)
 }
@@ -28,3 +29,22 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+
+def recipe_view(request, dish_name):
+    servings = int(
+        request.GET.get("servings", 1)
+    )  # Получение параметра servings, по умолчанию 1
+    recipe = DATA.get(dish_name)
+
+    if recipe is None:
+        return HttpResponse(f"Рецепт '{dish_name}' не найден.", status=404)
+
+    # Умножаем количество ингредиентов на servings
+    scaled_recipe = {
+        ingredient: amount * servings for ingredient, amount in recipe.items()
+    }
+
+    context = {"recipe": scaled_recipe}
+
+    return render(request, "calculator/index.html", context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import recipe_view
+
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path("<str:dish_name>/", recipe_view, name="recipe")
 ]
