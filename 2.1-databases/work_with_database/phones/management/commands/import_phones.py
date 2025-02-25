68e68b97abe22f07567d90406656d5ba9aa1e682
evgenii <vovk.evgenii.199811@gmail.com>
2025-02-25 10:27:55 +0300

    2.1

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,26 +1,32 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
-
     help = "Импорт телефонов из CSV"
 
     def handle(self, *args, **options):
-        with open("phones.csv", "r", encoding="utf-8") as file:
-            reader = csv.DictReader(file)
+        with open("phones.csv", newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
             for row in reader:
-                phone = Phone(
+                phone, created = Phone.objects.get_or_create(
                     id=row["id"],
-                    name=row["name"],
-                    image=row["image"],
-                    price=row["price"],
-                    release_date=row["release_date"],
-                    lte_exists=row["lte_exists"].lower() == "true",
+                    defaults={
+                        "name": row["name"],
+                        "price": row["price"],
+                        "image": row["image"],
+                        "release_date": row["release_date"],
+                        "lte_exists": row["lte_exists"].lower() == "true",
+                        "slug": slugify(row["name"]),
+                    },
                 )
-                phone.save()
-        self.stdout.write(self.style.SUCCESS("Импорт телефонов завершён!"))
+                if created:
+                    self.stdout.write(
+                        self.style.SUCCESS(f"Телефон {phone.name} добавлен")
+                    )
+                else:
+                    self.stdout.write(
+                        self.style.WARNING(f"Телефон {phone.name} уже существует")
+                    )
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render, redirect
 
 from phones.models import Phone
 
@@ -17,23 +17,3 @@ def show_product(request, slug):
     template = "product.html"
     context = {}
     return render(request, template, context)
-
-
-def phone_catalog(request):
-    # Получаем порядок сортировки из GET-параметров
-    sort_by = request.GET.get("sort", "name")  # По умолчанию сортируем по имени
-    if sort_by == "price_asc":
-        phones = Phone.objects.all().order_by("price")
-    elif sort_by == "price_desc":
-        phones = Phone.objects.all().order_by("-price")
-    else:
-        phones = Phone.objects.all().order_by("name")
-
-    context = {"phones": phones}
-    return render(request, "catalog.html", context)
-
-
-def phone_detail(request, slug):
-    phone = get_object_or_404(Phone, slug=slug)
-    context = {"phone": phone}
-    return render(request, "phone_detail.html", context)
