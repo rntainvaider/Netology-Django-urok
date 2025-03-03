bb271608c9aa164c070d95dbb661c648e4cc31cd
evgenii <vovk.evgenii.199811@gmail.com>
2025-03-03 09:46:53 +0300

    change 3.3

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,14 +1,14 @@
 from django_filters import rest_framework as filters
-from django_filters import DateFromToRangeFilter
-
+from django_filters import DateFromToRangeFilter, CharFilter
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
-    # TODO: задайте требуемые фильтры
     created_at = DateFromToRangeFilter()
+    status = CharFilter(field_name="status")
 
     class Meta:
         model = Advertisement
+        fields = ["created_at", "status"]
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,4 +1,7 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.throttling import UserRateThrottle
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Advertisement
@@ -6,18 +9,32 @@ from .serializers import AdvertisementSerializer
 from .filters import AdvertisementFilter
 
 
+class AdvertisementThrottle(UserRateThrottle):
+    rate = "10/min"
+
+
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = AdvertisementFilter
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    throttle_classes = [AdvertisementThrottle]
 
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
             return [IsAuthenticated()]
-        return []
+        return super().get_permissions()
+
+    def destroy(self, request, *args, **kwargs):
+        """Запрещаем удаление чужих объявлений."""
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response(
+                {"detail": "Удаление чужого объявления запрещено."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().destroy(request, *args, **kwargs)
